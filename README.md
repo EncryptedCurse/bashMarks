ca203f193d907977184d21f8776da753b373f844
EncryptedCurse <EncryptedCurse@users.noreply.github.com>
2019-04-01 00:20:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# bashMarks
+A shell script that allows you to bookmark and jump to commonly used directories.
+
+*Modernized combination of pedramamini's [lazy-cd](https://github.com/pedramamini/lazy-cd) and huyng's [bashmarks](https://github.com/huyng/bashmarks).*
+
+## Installation
+ 1. `wget https://raw.githubusercontent.com/EncryptedCurse/bashMarks/master/bashMarks.sh`
+ 2. `mkdir -p ~/.local/bin` (or an installation directory of your choice)
+ 3. `cp bashMarks.sh ~/.local/bin`
+ 4. Add the following line to `.bashrc`: `source ~/.local/bin/bashMarks.sh`
+
+## Usage
+A file named `.bmarks` will be created in your home directory. The name and path of your bookmark storage may be changed through the `FILE` variable at the top of the script.
+
+| command | alias | description |
+|---------|-------|-------------|
+|`save <name>`|`s`|save current directory|
+|`jump <name>`|`j`|jump to a bookmark|
+|`del  <name>`|`d`|delete a bookmark|
+|`list`|`l`|list all bookmarks|
+
+By default, aliases are enabled for easier use. These may be modified or removed entirely at the end of the script.
---
+++ bashMarks.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+FILE=~/.bmarks
+
+GRAY='\e[1;30m'
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+RESET='\e[0m'
+
+if [ ! -f $FILE ]; then
+    touch $FILE
+fi
+
+function save {
+    __help $1
+    if [[ -z $1 ]]; then
+        echo -e "${RED}no bookmark name provided${RESET}"
+    else
+        if grep -q "^$1|" $FILE; then
+            echo -ne "${RED}bookmark \"$1\" already exists, overwrite?${RESET} "
+            read REPLY
+            if [[ "${REPLY,,}" == "yes" || "${REPLY,,}" == "y" ]]; then
+                del $1 &> /dev/null
+            else
+                kill -INT $$
+            fi
+        fi
+        echo "$1|$PWD" >> $FILE
+        echo -e "${GREEN}bookmark for $PWD saved as \"$1\"${RESET}"
+    fi
+}
+
+function jump {
+    __help $1
+    if [[ -z $1 ]]; then
+        echo -e "${RED}no bookmark name provided${RESET}"
+    else
+        MARK=$(grep "^$1|" $FILE)
+        if [[ -z $MARK ]]; then
+            echo -e "${RED}bookmark \"$1\" does not exist${RESET}"
+        else
+            cd $(echo "$MARK|" | cut -d\| -f2)
+        fi
+    fi
+}
+
+function list {
+    __help $1
+    if [ -s $FILE ]; then
+        cat $FILE | sort | awk '{ printf "\033[1;33m%-18s\033[0m %s\n", $1, $2}' FS=\|
+    else
+        echo -e "${YELLOW}no bookmarks saved${RESET}"
+    fi
+}
+
+function del {
+    __help $1
+    MARK=$(grep "^$1|" $FILE)
+    if [[ -z $MARK ]]; then
+        echo -e "${RED}bookmark \"$1\" does not exist${RESET}"
+    else
+        \grep -v "^$1|" $FILE > $FILE.tmp
+        \mv $FILE.tmp $FILE
+        echo -e "${GREEN}deleted bookmark \"$1\"${RESET}"
+    fi
+}
+
+function __help {
+    if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+        echo -e "${BLUE}────────────── bashMarks ──────────────${RESET}
+save ${GRAY}<name>${RESET}     save current directory
+jump ${GRAY}<name>${RESET}     jump to a bookmark
+del  ${GRAY}<name>${RESET}     delete a bookmark
+list            list all bookmarks"
+        kill -INT $$
+    fi
+}
+
+alias s='save'
+alias j='jump'
+alias d='del'
+alias l='list'
